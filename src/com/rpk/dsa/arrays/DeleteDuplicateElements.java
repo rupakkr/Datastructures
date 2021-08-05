0ff090930bd7cd6986228389425d4328c4d8780b
Rupak <rupak.kr92@gmail.com>
2021-08-05 22:53:48 +0530

    Soleved - Remove Duplicate with Repeat Deletion Algo

---
+++ src/com/rpk/dsa/arrays/DeleteDuplicateElements.java
@@ -0,0 +1,35 @@
+package com.rpk.dsa.arrays;
+
+public class DeleteDuplicateElements {
+    public static int deleteDuplicate(int[] arr){
+        if(arr == null || arr.length == 0){
+            return 0;
+        }
+
+        if(arr.length == 1)
+            return 1;
+
+        int i = 1;
+        int j = 0;
+        int n = arr.length;
+
+        while(i < n){
+            if (arr[i-1] != arr[i]){
+                arr[j++] = arr[i-1];
+            }
+            i++;
+        }
+
+        arr[j++] = arr[n-1];
+
+        return j;
+
+    }
+        int[] arr = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        //0,1,2,3,4
+        System.out.println(deleteDuplicate(arr));
+
+        for (int i : arr)
+            System.out.println(i);
+    }
+}
